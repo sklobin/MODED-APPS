6dbecfb148dbeb846cbca6212674411b66becec2
sklobin <sherazk2k@gmail.com>
2026-01-31 12:13:41 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>SK Ultra 3D Apps</title>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<header>
+  <h1>🚀 SK ULTRA 3D APPS</h1>
+  <p>Next-Gen App Store Experience</p>
+</header>
+<marquee behavior="scroll" direction="left">
+<h1>  SHARAZ KHAN<h1>
+</marquee>
+
+<div class="apps-container">
+
+  <div class="card">
+    <div class="card-inner">
+      <img src="https://via.placeholder.com/120">
+      <h3>CapCut Pro</h3>
+      <span class="badge mod">MOD</span>
+      <p>Video Editing</p>
+      <a href="#">Download</a>
+    </div>
+  </div>
+
+  <div class="card">
+    <div class="card-inner">
+      <img src="https://via.placeholder.com/120">
+      <h3>Free Fire</h3>
+      <span class="badge normal">NORMAL</span>
+      <p>Action Game</p>
+      <a href="#">Download</a>
+    </div>
+  </div>
+
+</div>
+
+<script src="script.js"></script>
+</body>
+</html>
